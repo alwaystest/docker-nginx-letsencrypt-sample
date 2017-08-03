1f4fe89ad6d01c7f961a31c699605aa091cca81d
eric <ericceshi@163.com>
2017-08-03 02:29:28 -0400

    feature: complete eric reverse proxy https

--- samples/api/app.js
+++ samples/api/app.js
@@ -3,9 +3,21 @@ var cors = require('cors');
 
 var app = express();
 app.use(cors());
+app.use('/public',express.static('public'));
 
 app.get('/hello', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/status', function (req, res) {
+  res.json({ status: 'ok'});
+});
+
+app.get('/service.json', function (req, res) {
+  var rand = Math.floor(Math.random() * 3);
+  var url = '/public/service' + rand + '.json';
+  console.log(url);
+  res.redirect(url);
 });
 
 app.listen(3000, function () {
